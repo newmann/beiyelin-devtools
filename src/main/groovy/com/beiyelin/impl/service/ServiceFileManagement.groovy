b4a7e4859aaf36cce964d43a8f17a92566852f67
newmann <huxinsheng88@hotmail.com>
2023-11-12 20:52:47 +0800

    update 20231112

--- src/main/groovy/com/beiyelin/impl/service/ServiceFileManagement.groovy
+++ src/main/groovy/com/beiyelin/impl/service/ServiceFileManagement.groovy
@@ -1,10 +1,9 @@
 
-
 import org.slf4j.LoggerFactory
-
+import org.slf4j.Logger
 
 class ServiceFileManagement{
-    private final Logger logger= LoggerFactory.getLogger(getClass())
+    private final Logger logger= LoggerFactory.getLogger(ServiceFileManagement.getClass())
 
 
 
